406396bf2961b2bbc311af0185c039321fe5775f
Mammuasyir <mammuasyir0101@gmailcom>
2021-11-21 17:58:06 +0700

    Last fixed

--- resources/views/Auth/myregister.blade.php
+++ resources/views/Auth/myregister.blade.php
@@ -44,12 +44,12 @@
             <form action="/register" method="POST">
                 @csrf
                 <div class="mb-3">
-                    <label for="" class="form-label">Email address</label>
-                    <input type="email" name="email" class="form-control" id="" aria-describedby="emailHelp">
-                </div>
-                <div class="mb-3">
                     <label for="" class="form-label">Name</label>
                     <input type="text" name="name" class="form-control" id="">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">Email address</label>
+                    <input type="email" name="email" class="form-control" id="" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
                     <label for="" class="form-label">Password</label>
